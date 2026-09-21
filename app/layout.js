3219ef9f7eb25aebde1f7ec45e61791cb42aab5d
heydarrashid2015-spec <heydar.rashid2015@gmail.com>
2026-09-21 03:00:30 +0200

    Add app/layout.js

---
+++ app/layout.js
@@ -0,0 +1,3 @@
+import "./globals.css";
+export const metadata={title:"HusPass",description:"Dit hjem samlet ét sted"};
+export default function RootLayout({children}){return <html lang="da"><body>{children}</body></html>}
