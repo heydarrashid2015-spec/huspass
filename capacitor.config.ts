ad1234124c50b09fd333e6e84cfa0627d060facd
heydarrashid2015-spec <heydar.rashid2015@gmail.com>
2026-09-26 01:22:13 +0200

    Add HusPass mobile app configuration

---
+++ capacitor.config.ts
@@ -0,0 +1,11 @@
+const config = {
+  appId: "dk.huspass.app",
+  appName: "HusPass",
+  webDir: "out",
+  server: {
+    url: "https://huspass.dk",
+    cleartext: false
+  }
+};
+
+export default config;
